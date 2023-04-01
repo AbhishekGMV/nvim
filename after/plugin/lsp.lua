0f17e64165bcd3ea69d67ca93bc6be5ac8ddafaf
Abhishek Gowda M V <abhishek.mvg@gmail.com>
2023-04-01 23:11:08 +0530

    Add files via upload

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,16 @@
+local lsp = require('lsp-zero')
+lsp.preset("recommended")
+lsp.ensure_installed({
+	'tsserver',
+	'pyright',
+	'eslint',
+	'rust_analyzer',
+})
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+-- (Optional) Configure lua language server for neovim
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+lsp.setup()
---
+++ lua/noobmaster69/init.lua
@@ -0,0 +1,2 @@
+require("noobmaster69.remap")
+require("noobmaster69.set")
